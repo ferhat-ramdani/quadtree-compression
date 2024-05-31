f3813944f754901a8e9ef26ca09a1a80e55a176e
Ferhat Ramdani <ramdaniferhat22@gmail.com>
2024-05-31 20:08:55 +0200

    added mean color and modified some functions

--- c_tree.h
+++ c_tree.h
@@ -14,9 +14,10 @@ typedef struct _c_node {
 
 /*
 * Creates a node with the given data and children.
+* The function automatically updates the mean color of the node.
 * children must be allocated before calling this function.
 */
-c_node *create_c_node(color *color, c_node **children);
+c_node *create_c_node(c_node **children);
 
 /* Creates a leaf. */
 c_node *create_c_leaf(color *color);
@@ -24,8 +25,21 @@ c_node *create_c_leaf(color *color);
 /* Display the nodes of the c_tree t */
 void display_c_tree(c_node *t);
 
-/* Measures distance between two trees, suppose t1 and t2 not NULL */
-float c_distance(c_node *t1, c_node *t2);
+/* Measures exact distance between two trees, suppose t1 and t2 not NULL */
+float c_exact_distance(c_node *t1, c_node *t2);
+
+/* Measures mean distance between two trees, suppose t1 and t2 not NULL */
+float c_mean_distance(c_node *t1, c_node *t2);
+
+/* Returns the mean color of the colors of the given children nodes */
+color *mean_color(c_node **children);
+
+/* Stores the distances between every sub tree of the two trees in array distances */
+void sub_c_tree_distances(c_node *t1, c_node *t2, float *distances);
+
+
+/* Frees the leaf */
+void free_leaf(c_node *leaf);
 
 /* 
  * Free the memory allocated for the tree t
