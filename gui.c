e85c0f9b2463e59d0c7f5bc23fae7ef4e96feb09
Ferhat Ramdani <ramdaniferhat22@gmail.com>
2024-06-06 22:54:13 +0200

    added support for random shapes in visualisation

--- gui.c
+++ gui.c
@@ -8,11 +8,11 @@
 #include "minimizer.h"
 
 #define IMAGE_SIZE 512
+#define NB_BUTTONS 9
 #define WIDTH IMAGE_SIZE * 2
 #define HEIGHT IMAGE_SIZE * 3/2
 #define BUTTON_WIDTH WIDTH / 4
 #define BUTTON_HEIGHT HEIGHT / 15
-#define NB_BUTTONS 9
 #define MARGIN_LEFT WIDTH / 20
 #define BUTTON_MARGIN HEIGHT / 30
 #define MARGIN_TOP (HEIGHT - (BUTTON_HEIGHT * NB_BUTTONS + (NB_BUTTONS - 1) * BUTTON_MARGIN)) / 2
@@ -22,12 +22,9 @@
 #define INPUT_BOX_HEIGHT BUTTON_HEIGHT
 #define INPUT_BOX_X MARGIN_LEFT + BUTTON_WIDTH + 10
 #define INPUT_BOX_Y(y) (BUTTON_Y_POS(y))
-
 #define DRAW_INPUT_BOX(y, prompt, filename) \
   MLV_wait_input_box(INPUT_BOX_X, INPUT_BOX_Y(y), INPUT_BOX_X + INPUT_BOX_WIDTH, INPUT_BOX_HEIGHT, MLV_COLOR_WHITE, MLV_COLOR_BLACK, MLV_COLOR_GRAY, prompt, filename);
-
 #define BUTTON_Y_POS(y) (MARGIN_TOP + BUTTON_HEIGHT * (y) + BUTTON_MARGIN * (y))
-
 #define DRAW_BUTTON(y, text) \
   MLV_draw_text_box(MARGIN_LEFT, BUTTON_Y_POS(y), BUTTON_WIDTH, BUTTON_HEIGHT, text, 1, MLV_COLOR_WHITE, MLV_COLOR_WHITE, MLV_COLOR_BLACK, MLV_TEXT_CENTER, MLV_HORIZONTAL_CENTER, MLV_VERTICAL_CENTER)
 
@@ -87,20 +84,55 @@ c_node *create_c_tree_from_image(MLV_Image *image, int x, int y, int length) {
   return create_c_node(children);
 }
 
+void draw_shape(char shape, MLV_Color color, int x, int y, int length) {
+  if(shape == '*') {
+    int random = rand() % 4;
+    switch(random) {
+      case 0:
+        shape = 'r';
+        break;
+      case 1:
+        shape = 'c';
+        break;
+      case 2:
+        shape = 'x';
+        break;
+      case 3:
+        shape = '+';
+        break;
+    }
+  }
+  switch(shape) {
+    case 'r':
+      MLV_draw_rectangle(x, y, length, length, color);
+      break;
+    case 'c':
+      MLV_draw_circle(x + length/2, y + length/2, length/2, color);
+      break;
+    case '-':
+      MLV_draw_line(x, y + length/2, x + length, y + length/2, color);
+      break;
+    case '|':
+      MLV_draw_line(x + length/2, y, x + length/2, y + length, color);
+      break;
+    case 'x':
+      MLV_draw_line(x, y, x + length, y + length, color);
+      MLV_draw_line(x, y + length, x + length, y, color);
+      break;
+    case '+':
+      MLV_draw_line(x + length/2, y, x + length/2, y + length, color);
+      MLV_draw_line(x, y + length/2, x + length, y + length/2, color);
+      break;
+    default:
+      MLV_draw_filled_rectangle(x, y, length, length, color);
+      break;
+  }
+} 
+
 void draw_c_tree_as_image_aux(c_node *c_tree, int x, int y, int length, char shape) {
   if (c_tree->children == NULL) {
     MLV_Color color = MLV_rgba(c_tree->color->red, c_tree->color->green, c_tree->color->blue, c_tree->color->alpha);
-    switch(shape) {
-      case 'r':
-        MLV_draw_rectangle(x, y, length, length, color);
-        break;
-      case 'c':
-        MLV_draw_circle(x + length/2, y + length/2, length/2, color);
-        break;
-      default:
-        MLV_draw_filled_rectangle(x, y, length, length, color);
-        break;
-    }
+    draw_shape(shape, color, x, y, length);
   } else {
     draw_c_tree_as_image_aux(c_tree->children[0], x, y, length/2, shape);
     draw_c_tree_as_image_aux(c_tree->children[1], x + length/2, y, length/2, shape);
@@ -179,25 +211,25 @@ void split_leaves_by_err(c_node *c_tree, MLV_Image *image, int x, int y, int len
  * are below a certain reference error = (min_err + max_err) / 3
  * where min_err and max_err are the minimum and maximum errors of the leaves
  */
-float step_approximate(c_node *c_tree, MLV_Image *image, int delay) {
+float step_approximate(c_node *c_tree, MLV_Image *image, int delay, char shape) {
   float min_err = FLT_MAX;
   float max_err = FLT_MIN;
   get_min_max_err(c_tree, image, 0, 0, MLV_get_image_width(image), &min_err, &max_err);
   float ref_err = (min_err + max_err) / 3;
   split_leaves_by_err(c_tree, image, 0, 0, MLV_get_image_width(image), ref_err);
-  draw_c_tree_as_image(c_tree, 'j');
+  draw_c_tree_as_image(c_tree, shape);
   MLV_wait_milliseconds(delay);
   return ref_err;
 }
 
-c_node *approximate_image(MLV_Image *image) {
+c_node *approximate_image(MLV_Image *image, char shape) {
   color *c = average_color(image, 0, 0, MLV_get_image_width(image), MLV_get_image_height(image));
   c_node *c_tree = create_c_leaf(c);
   int delay = 10;
   float err = FLT_MAX;
   int step = 0;
   while(err > 0.00001) {
-    err = step_approximate(c_tree, image, delay);
+    err = step_approximate(c_tree, image, delay, shape);
     printf("Step %d, err %.2f\n", step, err);
     step++;
   }
@@ -214,20 +246,18 @@ void handle_buttons(MLV_Image **image, c_node **tree) {
     if (x >= MARGIN_LEFT && x <= MARGIN_LEFT + BUTTON_WIDTH) {
       if (y >= BUTTON_Y_POS(0) && y <= BUTTON_Y_POS(0) + BUTTON_HEIGHT) {
         // Load Image
-        if (*image != NULL) {
+        if (*image != NULL)
           MLV_free_image(*image);
-        }
         DRAW_INPUT_BOX(0, "Enter image filename: ", &filename);
         *image = MLV_load_image(filename);
         if (*image != NULL) {
           MLV_resize_image_with_proportions(*image, IMAGE_SIZE, IMAGE_SIZE);
           MLV_draw_image(*image, X_ORIGIN, Y_ORIGIN);
-          MLV_update_window();
         }
       } else if (y >= BUTTON_Y_POS(1) && y <= BUTTON_Y_POS(1) + BUTTON_HEIGHT) {
         // Quadtree Approximation
         if (*image != NULL) {
-          *tree = approximate_image(*image);
+          *tree = approximate_image(*image, '*');
         }
       } else if (y >= BUTTON_Y_POS(2) && y <= BUTTON_Y_POS(2) + BUTTON_HEIGHT) {
         // Save Binary BW
@@ -235,7 +265,6 @@ void handle_buttons(MLV_Image **image, c_node **tree) {
           DRAW_INPUT_BOX(2, "Enter filename to save (BW): ", &filename);
           save_c_tree_binary(filename, *tree);
           draw_text("Quadtree saved (BW)", MLV_COLOR_YELLOW);
-          MLV_update_window();
         }
       } else if (y >= BUTTON_Y_POS(3) && y <= BUTTON_Y_POS(3) + BUTTON_HEIGHT) {
         // Save Binary RGBA
@@ -243,14 +272,12 @@ void handle_buttons(MLV_Image **image, c_node **tree) {
           DRAW_INPUT_BOX(3, "Enter filename to save (RGBA): ", &filename);
           save_c_tree_binary(filename, *tree);
           draw_text("Quadtree saved (RGBA)", MLV_COLOR_YELLOW);
-          MLV_update_window();
         }
       } else if (y >= BUTTON_Y_POS(4) && y <= BUTTON_Y_POS(4) + BUTTON_HEIGHT) {
         // Minimize Quadtree
         if (*tree != NULL) {
           minimize_identical_leaves_in_node(*tree);
           draw_text("Quadtree minimized", MLV_COLOR_YELLOW);
-          MLV_update_window();
         }
       } else if (y >= BUTTON_Y_POS(5) && y <= BUTTON_Y_POS(5) + BUTTON_HEIGHT) {
         // Save Minimized BW
@@ -258,7 +285,6 @@ void handle_buttons(MLV_Image **image, c_node **tree) {
           DRAW_INPUT_BOX(5, "Enter filename to save (Minimized BW): ", &filename);
           save_c_tree_binary(filename, *tree);
           draw_text("Quadtree saved (Minimized BW)", MLV_COLOR_YELLOW);
-          MLV_update_window();
         }
       } else if (y >= BUTTON_Y_POS(6) && y <= BUTTON_Y_POS(6) + BUTTON_HEIGHT) {
         // Save Minimized RGBA
@@ -266,7 +292,6 @@ void handle_buttons(MLV_Image **image, c_node **tree) {
           DRAW_INPUT_BOX(6, "Enter filename to save (Minimized RGBA): ", &filename);
           save_c_tree_binary(filename, *tree);
           draw_text("Quadtree saved (Minimized RGBA)", MLV_COLOR_YELLOW);
-          MLV_update_window();
         }
       } else if (y >= BUTTON_Y_POS(7) && y <= BUTTON_Y_POS(7) + BUTTON_HEIGHT) {
         // Open Image
@@ -274,10 +299,10 @@ void handle_buttons(MLV_Image **image, c_node **tree) {
         *tree = load_c_tree_binary(filename);
         if (*tree != NULL) {
           draw_text("Quadtree loaded", MLV_COLOR_YELLOW);
-          draw_c_tree_as_image(*tree, 'r');
-          MLV_update_window();
+          draw_c_tree_as_image(*tree, '*');
         }
       }
+      MLV_update_window();
     }
   }
 }
